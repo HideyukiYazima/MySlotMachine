9498d84befc6c22a33eb35225801b90e1aa60606
Yazima <yazima04200@gmail.com>
2022-11-10 23:01:44 +0900

    スロットstop機能実装

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 {
   class Panel {
@@ -7,11 +7,16 @@
       section.classList.add('panel');
 
       this.img = document.createElement('img');
-      this.img.src = 'img/seven.png';
+      this.img.src = this.getRandomImage();
+
+      this.timeoutId = undefined;
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+      this.stop.addEventListener('click', () => {
+        clearTimeout(this.timeoutId);
+      });
 
       section.appendChild(this.img);
       section.appendChild(this.stop);
@@ -31,6 +36,9 @@
 
     spin() {
       this.img.src = this.getRandomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
     }
   }
 
@@ -43,7 +51,7 @@
   const spin = document.getElementById('spin');
   spin.addEventListener('click', () => {
     panels.forEach(panel => {
-      panel.spin
+      panel.spin();
     });
   });
 }
